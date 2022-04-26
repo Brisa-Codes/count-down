33bcdc0788022e51f1612f13e3b9fa99f213afe6
Brisa-Codes <brisamukunde1@gmail.com>
2022-04-26 17:00:10 +0300

    js functionality

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-const time = 143;
-const countDown = time * 60;
-const counting = document.getElementById 
+const countdown = () => {
+    const countDate = new Date("April 31, 2022 00:00:00").getTime();
+    const now = new Date().getTime()
+    const gap = countDate - now;
+
+    // calculating time 
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // exact time
+    const dayText = Math.floor(gap / day);
+    const hourText = Math.floor((gap % day) / hour);
+    const minuteText = Math.floor((gap % hour) / minute);
+    const secondText = Math.floor((gap % minute) / second);
+
+    // manipulate the countdown
+    document.querySelector(".day").innerHTML = dayText;
+    document.querySelector(".hour").innerHTML = hourText;
+    document.querySelector(".minute").innerHTML = minuteText;
+    document.querySelector(".seconds").innerHTML = secondText;
+};
+
+setInterval(countdown, 1000);
